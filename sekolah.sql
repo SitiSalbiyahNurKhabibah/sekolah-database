72642684ff77823fa0cc632d8078d31d23921aa1
Siti Salbiyah Nur Khabibah <salbiyakhabibah@gmail.com>
2025-10-02 15:47:51 +0700

    Query data siswa dan nilai

--- sekolah.sql
+++ sekolah.sql
@@ -1,18 +1,8 @@
-INSERT INTO siswa (nama, umur, jurusan) VALUES
-('Andi', 16, 'IPA'),
-('Budi', 17, 'IPS'),
-('Citra', 16, 'IPA'),
-('Dewi', 17, 'Bahasa'),
-('Eko', 16, 'IPS');
+SELECT * FROM siswa;
 
-INSERT INTO nilai (siswa_id, mata_pelajaran, nilai) VALUES
-(1, 'Matematika', 85),
-(1, 'Bahasa Inggris', 88),
-(2, 'Matematika', 75),
-(2, 'Bahasa Inggris', 90),
-(3, 'Fisika', 80),
-(3, 'Kimia', 82),
-(4, 'Bahasa Indonesia', 89),
-(4, 'Sejarah', 78),
-(5, 'Ekonomi', 84),
-(5, 'Sosiologi', 86);
+SELECT nama, jurusan FROM siswa WHERE jurusan = 'IPA';
+
+SELECT siswa.nama, AVG(nilai.nilai) AS rata_nilai
+FROM siswa
+JOIN nilai ON siswa.id = nilai.siswa_id
+GROUP BY siswa.nama;
